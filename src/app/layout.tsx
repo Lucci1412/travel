035e5cbf7b7bb6ba5853d599f029cdef6a70b829
Lucci1412 <66712654+Lucci1412@users.noreply.github.com>
2024-09-21 23:25:34 +0700

    setup project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "Travel",
+  description: "Travel abc",
 };
 
 export default function RootLayout({
